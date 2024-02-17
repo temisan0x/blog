43056f1bd6aa830bda92f73840e72deb1117bfb4
temycodes <temycodes@gmail.com>
2024-02-17 17:04:20 +0100

    code cleanup

--- app/api/hello/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export async function GET() {
-    try {
-      return new Response("Hello world")
-    } catch (error) {
-      console.error(error);
-    }
-  }
