31f29302f241da4c3a8f25248d95a72eb613981c
temycodes <temycodes@gmail.com>
2024-02-17 17:08:33 +0100

    code cleanup

--- components/PageLoader.tsx
+++
@@ -1,9 +0,0 @@
-const PageLoader = () => {
-    return (
-      <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
-        <div className="animate-spin rounded-full border-t-4 border-b-4 border-blue-500 h-12 w-12"></div>
-      </div>
-    );
-  };
-  
-  export default PageLoader;
