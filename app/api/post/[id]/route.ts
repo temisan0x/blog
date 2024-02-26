aea07608c26980fe904b74689e13b3a795d275e8
temycodes <temycodes@gmail.com>
2024-02-26 16:07:17 +0100

    feat: added prisma to delete functionality

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -1,7 +1,11 @@
+import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export function DELETE(request: Request, { params }: any) {
+export async function DELETE(request: Request, { params }: any) {
   const id = params.id;
+  const post = await prisma.post.delete({
+    where:{id}
+  })
   console.log({id});
   return NextResponse.json(request);
 }
