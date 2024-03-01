eace11f3fe7dfb4a48df8616bdbf991c7f1d93d7
temycodes <temycodes@gmail.com>
2024-03-01 14:55:14 +0100

    build: register endpoint

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,3 +1,35 @@
-export const POST =(req: Request)=> {
-    
-}
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { connnectToDb } from "@/helpers/server-helpers";
+import prisma from "@/prisma";
+
+export const POST = async (req: Request) => {
+  try {
+    const { name, email, password } = await req.json();
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { message: "Invalid username or password" },
+        { status: 422 }
+      );
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await connnectToDb();
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+    return NextResponse.json({
+      message: "User created successfully",
+      user: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Server error" }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
