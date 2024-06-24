64e2fda76a68e38b526fa17923bea0aa98732f9f
temycodes <temycodes@gmail.com>
2024-06-24 11:01:35 +0100

    build: added next-mdx-remote ✨

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,37 @@ import "@/app/global.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import NextAuthProvider from "./components/NextAuthProvider";
-import { Schibsted_Grotesk } from 'next/font/google'
+import {GeistSans} from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import 
 
 export const metadata: Metadata = {
-  title: "",
-  description: "A personal blog from yours sincerly Temisan0x.",
-};
-
-// const schibsted = Schibsted_Grotesk({subsets: ['latin']})
-
+  metadataBase: new URL(baseUrl),
+  title: {
+      default: '.dir-diare',
+      template: '%s | .dir-diare',
+  },
+  description: 'This is my dir.',
+  openGraph: {
+      title: 'My Dir',
+      description: 'This is my dir.',
+      url: baseUrl,
+      siteName: 'My Dir',
+      locale: 'en_US',
+      type: 'website',
+  },
+  robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+          index: true,
+          follow: true,
+          'max-video-preview': -1,
+          'max-image-preview': 'large',
+          'max-snippet': -1,
+      },
+  },
+}
 export default async function RootLayout({
   children,
 }: {
