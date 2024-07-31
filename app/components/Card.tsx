dd1550bc437c731a8d3a77fc64663b559c7ff853
temycodes <temycodes@gmail.com>
2024-07-31 08:44:24 +0100

    chore: code cleanup

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -26,12 +26,14 @@ export default function Card({
             animate={{ opacity: 1 }}
             initial={{ opacity: 0 }}
         >
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 border border-slate-800 p-4 rounded-md shadow-md transition-shadow duration-300 hover:bg-gray-500/5 ease-in-out">
-                <div className="flex justify-center items-center relative overflow-clip">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 border border-slate-800 p-4 rounded-md shadow-md transition-shadow duration-300 hover:bg-gray-500/5 ease-in-out overflow-clip">
+                <div className="flex justify-center items-center relative overflow-clip h-32 ">
                     <Image
                         alt={title}
                         src={projectImg}
-                        className="inset-0  object-cover bg-clip-border bg-black shadow-lg absolute h-full w-full group-hover:scale-125 duration-300"
+                        className="inset-0 object-cover bg-clip-border bg-black shadow-lg absolute h-full w-full group-hover:scale-125 duration-300"
+                        // width={544}
+                        // height={306}
                         fill={true}
                     />
                 </div>
@@ -40,16 +42,16 @@ export default function Card({
                         {title}
                     </h4>
                     <p
-                        className="text-sm prose truncate tracking-tighter"
+                        className="text-sm prose truncate overflow-clip tracking-tighter"
                         title={description}
                     >
                         {description}
                     </p>
                     <div
-                        className="text-sm  overflow-hidden"
+                        className="text-sm truncate w-80 overflow-hidden"
                         title={projectType}
                     >
-                        <p className="bg-slate-700 p-1 rounded-sm inline truncate">
+                        <p className="bg-slate-700 p-1 rounded-sm inline">
                             {projectType}
                         </p>
                     </div>
