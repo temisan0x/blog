cfd0ed8e2670881fedfc64c9bad08d2293b86b66
temycodes <temycodes@gmail.com>
2024-08-13 20:51:40 +0100

    minor fixes in blog images

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -36,19 +36,21 @@ const BlogPosts = async () =>  {
                       href={`/blog/${post.slug}`}
                   >
                       <div className="flex p-4 mb-4  bg-zinc-900 border border-transparent rounded-md duration-200 cursor-pointer hover:translate-x-3 hover:dark:border-neutral-600/50">
-                          <div className="relative justify-items-center">
-                              <div className="w-16 h-16 rounded-full overflow-hidden">
-                                  <Image
-                                      src={
-                                          post.metadata.image ||
-                                          '/images/bg-noise.png'
-                                      }
-                                      alt="Background"
-                                      className="w-full h-full rounded-full object-contain overflow-hidden"
-                                      fill={true}
-                                  />
-                              </div>
-                          </div>
+                      <div className="relative justify-items-center">
+                                <div className="w-16 h-16 overflow-hidden rounded-full">
+                                    <Image
+                                        src={
+                                            post.metadata.image ||
+                                            '/images/bg-noise.png'
+                                        }
+                                        alt="Background"
+                                        className="w-full h-full rounded-full object-cover"
+                                        width={200}
+                                        height={200}
+                                        quality={90}
+                                    />
+                                </div>
+                            </div>
                           <div className="flex flex-col w-full place-self-center ml-2">
                               <div className="font-semibold">
                                   {post.metadata.title}
