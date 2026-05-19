d685a090ff745ec9b6a8f062c24fa876216fadda
temycodes <temycodes@gmail.com>
2026-05-19 11:38:50 +0100

    build: update content and mdx components

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,14 @@
 import { getBlogPosts } from "./blog/utils";
 
-export const baseUrl = "https://temisan0x.vercel.app/";
+export const baseUrl = "https://temisan0x.vercel.app";
 
-export default async function sitemap (){
+export default async function sitemap() {
     let blogs = getBlogPosts().map((post) => ({
         url: `${baseUrl}/blog/${post.slug}`,
         lastModified: post.metadata.publishedAt,
     }));
 
-    let routes = ['', '/blog', 'work', '/about'].map((route)=> ({
+    let routes = ['', '/blog', '/work', '/about'].map((route) => ({
         url: `${baseUrl}${route}`,
         lastModified: new Date().toISOString().split('T')[0],
     }));
